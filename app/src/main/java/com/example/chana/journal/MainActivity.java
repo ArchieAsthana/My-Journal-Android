148f57b44760ab1fa0378735cce76ecbf12bb613
archana srivastava <srivastavaarchana0991@gmail.com>
2017-04-10 00:18:44 -0400

    Update UI colors.

--- app/src/main/java/com/example/chana/journal/MainActivity.java
+++ app/src/main/java/com/example/chana/journal/MainActivity.java
@@ -1,65 +1,45 @@
 package com.example.chana.journal;
 
-import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    private EditText Username;
+    private EditText usernameEditText;
     private EditText passEditText;
-    private Button Login;
+    private Button loginButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Username = (EditText) findViewById(R.id.UsernameEditText);
+        usernameEditText = (EditText) findViewById(R.id.UsernameEditText);
         passEditText = (EditText) findViewById(R.id.passEditText);
-        Login = (Button) findViewById(R.id.btLogin);
-        Login.setOnClickListener(this);
-        addListenerOnButton();
-    }
-    public void addListenerOnButton() {
-
-        final Context context = this;
-
-        Login = (Button) findViewById(R.id.btLogin);
-
-        Login.setOnClickListener(new View.OnClickListener(){
-
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(context,Main2Activity.class);
-                startActivity(intent);
-
-            }
-        });
+        loginButton = (Button) findViewById(R.id.btLogin);
+        loginButton.setOnClickListener(this);
     }
 
-    public void onClick(View v){
+    public void onClick(View v) {
 
-        if(!validate()) {
-            Toast.makeText(MainActivity.this,"SignIn Successful",Toast.LENGTH_LONG).show();
+        if (validateLogin()) {
+            Toast.makeText(MainActivity.this, "Login Successful", Toast.LENGTH_LONG).show();
+            Intent intent = new Intent(this, Main2Activity.class);
+            startActivity(intent);
         }
     }
 
-    public boolean validate() {
-        if (Username.getText().toString().trim().length() <= 0) {
-
-            Toast.makeText(MainActivity.this,"Please Enter Username",Toast.LENGTH_SHORT).show();
-            return true;
-        } else if (passEditText.getText().toString().trim().length() <= 0) {
-
-            Toast.makeText(MainActivity.this,"Please Enter Password",Toast.LENGTH_SHORT).show();
-
-            return true;
-
+    public boolean validateLogin() {
+        if (usernameEditText.getText().toString().trim().isEmpty()) {
+            Toast.makeText(MainActivity.this, "Incorrect Username", Toast.LENGTH_SHORT).show();
+            return false;
+        } else if (passEditText.getText().toString().trim().isEmpty()) {
+            Toast.makeText(MainActivity.this, "Incorrect Password", Toast.LENGTH_SHORT).show();
+            return false;
         }
-        return false;
+        return true;
     }
 }
